ff74381bceca186e8ca4d36dee2d32a98d4fcef2
Amani Mildred Garvin <amani3moon@gmail.com>
2019-07-29 10:47:09 -0400

    changed to write coordinate vectors and values in multiple csvs and directories

--- jov/jov_bfields/write_jrm09.py
+++ jov/jov_bfields/write_jrm09.py
@@ -14,85 +14,6 @@ import os
 #############################################
 rj = 71492000 #m
 
-#-----------------------------------------
-# Testing Linear Interpolation in AMRVAC
-# With Dipole Field Example
-#-----------------------------------------
-'''
-mult = 1
-Br, Btheta, Bphi, Bmag, lat_, lon_ = solve_jrm09.dipole(mult)
-
-#-----------------------------------------
-# Plot Dipole Field and Save
-#-----------------------------------------
-#set up plotting devices
-fig = plt.figure(figsize=(8.0, 8.0))
-ax = fig.add_subplot(111, projection='3d')
-
-#plot jupiter wireframe
-theta, phi = np.mgrid[0:2*np.pi:20j,0:np.pi:10j]
-
-rj = 71492000#[m]
-xj = rj*np.cos(theta)*np.sin(phi)
-yj = rj*np.sin(theta)*np.sin(phi)
-zj = rj*np.cos(phi)
-ax.plot_wireframe(xj,yj,zj,color='C1') #the orange spot!
-
-x = r*np.cos(lat_)*np.sin(lon_)
-y = r*np.sin(lat_)*np.sin(lon_)
-z = r*np.cos(lon_)
-Bx = Br * np.cos(Btheta) * np.sin(Bphi)
-By = Br * np.sin(Btheta) * np.sin(Bphi)
-Bz = Br * np.cos(Bphi)
-
-plt.title('Radial Component of Dipole Field')
-l_arrow = 10**10
-ax.quiver(x, y, z, Bx, By, Bz, length = l_arrow, normalize=True)
-plt.show()
-
-'''
-
-# Understanding the Output Arrays
-#---------------------------------------
-# Br, Btheta, Bphi are 2D arrays
-# solving for the field at r=const
-# B_[lat_, lon_]
-# size = 22x22
-
-# lat and lon are vectors of length 22
-# lat = [-81.81....90.0, .136 spacing] #in degrees
-# lon = [0.0.....343.63, .272 spacing] #in degrees
-
-
-
-
-
-#########################
-# Writes JRM09 to CSV: Not efficient at all!
-#########################
-'''
-mults = np.linspace(1, 10**10,22)
-rs = np.linspace(rj, 10**11, 22)
-with open('dipole_grid_sphere_2.csv', mode = 'w') as csvfile:
-	jrm09writer = csv.writer(csvfile, delimiter = ',')
-	for r in rs:
-		#print(r)
-		#mag, Br, Btheta, Bphi, Bmag, Vp, lat, lon = solve_jrm09.jrm09(r)
-		Br, Btheta, Bphi, Bmag, lat, lon = solve_jrm09.dipole(r)
-		for i in range(lat.size):
-			#get lon
-			theta = lat[i]
-			for j in range(lon.size):
-				#get br, btheta, bphi
-				br = Br[i, j]
-				btheta = Btheta[i, j]
-				bphi = Bphi[i, j]
-
-				#lat
-				phi = lon[j]
-
-				jrm09writer.writerow([r, theta, phi, br, btheta, bphi])
-'''
 
 ############################
 # Write JRM09 to CSV grids
@@ -100,20 +21,94 @@ with open('dipole_grid_sphere_2.csv', mode = 'w') as csvfile:
 
 
 #Create a folder titled JRM09_GRID
+jrm09 = '/Users/amani3g/jupiter/amrvac/jov/jov_bfields/JRM09_GRID'
+os.mkdir(jrm09)
 
-# Create JRM09_Vectors.csv
-# Write coordinate vectors to JRM09_Vectors.csv as columns, eg:
-# r_vec, phi_vec, theta_vec
-# r1, phi1, theta1
-# r2, phi2, theta2
-# rN, phiN, thetaN
+#Solve for Vectors
+r_vec = np.linspace(rj, rj*10**3, 22)
+theta_vec = solve_jrm09.jrm09(rj)[6] 
+phi_vec = solve_jrm09.jrm09(rj)[7]
 
+# 1: Create JRM09_Vectors.csv
+jrm09_vectors = jrm09 + '/JRM09_Vectors.csv'
+with open(jrm09_vectors, mode=w) as vector_file:
+	vector_writer = csv.writer(vector_file, delimeter = ',')
+
+	# Write coordinate vectors to JRM09_Vectors.csv as columns, eg:
+	# r_vec, phi_vec, theta_vec
+	# r1, phi1, theta1
+	# r2, phi2, theta2
+	# .
+	# .
+	# .
+	# rN, phiN, thetaN
+
+	for i in range(r_vec.size):
+		vector_writer.writerow([r_vec[i], theta_vec[i], phi_vec[i]])
 
 # 2: For each r in r_vec
-# Create a folder r_surface
-# Go into the folder r_surface
-# Solve for Br, Btheta, Bphi, Bmag 
-# Write Br, Btheta, Bphi, Bmag to csvs, the values iterate as B[theta, phi]
+r_vec = np.linspace(rj, rj*10**3, 22)
+for r in r_vec:
+	mult = int(r/rj)
+	r_surface_dir = jrm09 + '/' + str(mult)+'_surface'
+	# Create a folder r_surface
+	os.mkdir(r_surface_dir)
+	# Solve for Br, Btheta, Bphi, Bmag
+	mag, Br, Btheta, Bphi, Bmag, Vp, lat, lon = solve_jrm09.jrm09(r)
+
+	# Write Br, Btheta, Bphi, Bmag to csv
+
+	# First write Br r_surface/Br.csv
+	Br_values = r_surface_dir + '/Br.csv'
+	with open(Br_values, mode=w) as Br_file:
+		Br_writer = csv.writer(Br_file, delimeter=',')
+		
+		# The values iterate as Br[theta, phi], eg:
+		# Br[theta1, phi1], Br[theta1, phi2], Br[theta1, phi3],...., Br[theta1, phiN]
+		# Br[theta2, phi1], Br[theta2, phi2], Br[theta2, phi3],...., Br[theta2, phiN]
+		# .
+		# . 	 	 	 	 	 	Br[theta(i), phi(j)]
+		# .														.
+		# Br[thetaN, phi1], Br[thetaN, phi2], Br[thetaN, phi3],...., Br[thetaN, phiN]
+
+		for i in range(theta_vec.size):
+			Br_writer.writerow([Br[i,]])
+
+	# Then write Btheta r_surface/Btheta.csv
+	Btheta_vales = r_surface_dir + '/Btheta.csv'
+	with open(Btheta_vales, mode=w) as Btheta_file:
+		Btheta_writer = csv.writer(Btheta_file, delimeter = ',')
+
+		# The values iterate as Btheta[theta, phi], eg:
+		# Btheta[theta1, phi1], Btheta[theta1, phi2], Btheta[theta1, phi3],...., Btheta[theta1, phiN]
+		# Btheta[theta2, phi1], Btheta[theta2, phi2], Btheta[theta2, phi3],...., Btheta[theta2, phiN]
+		# .
+		# . 	 	 	 	 	 	.
+		# .														.
+		# Btheta[thetaN, phi1], Btheta[thetaN, phi2], Btheta[thetaN, phi3],...., Btheta[thetaN, phiN]
+
+		for i in range(theta_vec.size):
+			Br_writer.writerow([Btheta[i,]])
+
+	# Last write Bphi r_surface/Bphi.csv
+	Bphi_values = r_surface_dir + '/Bphi.csv'
+	with open(Bphi_values, mode=w) as Bphi_file:
+		Bphi_writer = csv.writer(Bphi_file, delimeter = ',')
+		# The values iterate as Bphi[theta, phi], eg:
+		# Bphi[theta1, phi1], Bphi[theta1, phi2], Bphi[theta1, phi3],...., Bphi[theta1, phiN]
+		# Bphi[theta2, phi1], Bphi[theta2, phi2], Bphi[theta2, phi3],...., Bphi[theta2, phiN]
+		# .
+		# . 	 	 	 	 	 	.
+		# .														.
+		# Bphi[thetaN, phi1], Bphi[thetaN, phi2], Bphi[thetaN, phi3],...., Bphi[thetaN, phiN]
+
+		for i in range(theta_vec.size):
+			Bphi_writer.writerow([Bphi[i,]])
+		
+
+
+
+
 
 
 
